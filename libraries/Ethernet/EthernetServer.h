33ba4d486cbcdd83d47a0385c5d7c9da3d5317db
plbyrd <plbyrd@gmail.com>
2013-12-01 22:16:52 -0600

    Custom Ethernet files

---
+++ libraries/Ethernet/EthernetServer.h
@@ -0,0 +1,24 @@
+#ifndef ethernetserver_h
+#define ethernetserver_h
+
+#include "Server.h"
+
+class EthernetClient;
+
+class EthernetServer : 
+public Server {
+private:
+  uint16_t _port;
+  void accept();
+public:
+  EthernetServer(uint16_t);
+  EthernetClient available();
+  EthernetClient connected();
+  EthernetClient connected(uint8_t sock);
+  virtual void begin();
+  virtual size_t write(uint8_t);
+  virtual size_t write(const uint8_t *buf, size_t size);
+  using Print::write;
+};
+
+#endif
